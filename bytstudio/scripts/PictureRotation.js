c8d105ab62794339ad36c24be6d6ce5702aa05a0
htmlcssjavascriptcoursera <h1609397@mvrht.com>
2017-04-19 21:03:09 +0200

    Picture rotation done.

---
+++ bytstudio/scripts/PictureRotation.js
@@ -0,0 +1,19 @@
+var imageCount  = 15;
+var imageNumber = 15;
+
+function switchImage() {
+	var message = "<img src=\"./pictures/" + imageNumber + ".jpg\">";
+	document.getElementById("imageHolder").innerHTML = message;
+	if(imageNumber < imageCount) {
+		imageNumber += 1;
+	}
+	else {
+		imageNumber = 1;
+	}
+}
+
+/* setInterval calls a function within provided time range. 
+   This time 9000ms is set as default.
+*/
+setInterval('switchImage()', 9000);
+
