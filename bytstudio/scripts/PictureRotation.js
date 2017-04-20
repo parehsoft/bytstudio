6af01de9ead9bebdef08690448d3c6f2a25434b5
htmlcssjavascriptcoursera <h1609397@mvrht.com>
2017-04-20 18:36:46 +0200

    aj

--- bytstudio/scripts/PictureRotation.js
+++ bytstudio/scripts/PictureRotation.js
@@ -20,7 +20,7 @@ setInterval('switchImage()', 9000);
 
 /*** LOGO MOUSEOVER ZOOM --------------------------------------------------------------------------------------------- */
 function zoomLogoIn() {
-	var message = "<img id=\"zoomedLogo\" src=\"./pictures/bytstudio-big.jpg\">";
+	var message = "<img id=\"zoomedLogo\" src=\"./pictures/bytstudio-1980.jpg\">";
 	var element = document.getElementById("forZoomedLogo");
 	element.innerHTML = message;
 }
