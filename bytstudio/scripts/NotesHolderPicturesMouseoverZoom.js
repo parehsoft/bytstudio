5cd7d0f0cdbce123ded02127de02b9bed69a7fa0
htmlcssjavascriptcoursera <h1609397@mvrht.com>
2017-08-19 18:45:44 +0200

    remaning file

---
+++ bytstudio/scripts/NotesHolderPicturesMouseoverZoom.js
@@ -0,0 +1,14 @@
+/*** IMAGE MOUSEOVER ZOOM --------------------------------------------------------------------------------------------- */
+
+function zoomBytstudioIn() {
+    var element = document.getElementById("displayImageFromInfoHolder");
+    element.style.visibility = "visible";
+}
+
+function zoomBytstudioOut() {
+    var element = document.getElementById("displayImageFromInfoHolder");
+    element.style.visibility = "hidden";
+}
+
+document.querySelector("#infoHolderImageWrapper").onmouseover = zoomBytstudioIn;
+document.querySelector("#infoHolderImageWrapper").onmouseout = zoomBytstudioOut;
