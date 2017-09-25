9bd2ab0e0717b1b3e59bbeea1237ae33f2d41bfc
htmlcssjavascriptcoursera <h1609397@mvrht.com>
2017-09-25 20:59:26 +0200

    show loading rectified

--- bytstudio/scripts/UseAjax.js
+++ bytstudio/scripts/UseAjax.js
@@ -1,69 +1,86 @@
 (function (global) { // IIFE
     var UseAjax = {}; // Namespace.
 
+    function ShowLoading() {
+        document.querySelector("#contentPlaceHolder").innerHTML = "<div id=\"imageHolder\"><div style=\"margin-left: 49%;\"><img src=\"pictures/ajax-loader.gif\"></div></div>";
+    }
+
     function HandleResponse(request) {
-	    var element = document.querySelector("#contentPlaceHolder");
-	    element.innerHTML = "<div id=\"imageHolder\"><div style=\"margin-left: 49%;\"><img src=\"pictures/ajax-loader.gif\"></div></div>";
-        element.innerHTML = request.responseText;
+	    document.querySelector("#contentPlaceHolder").innerHTML = request.responseText;
     }
 
     UseAjax.LoadNetCurtains = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/netCurtains.html", HandleResponse);
     };
 
     UseAjax.LoadCurtains = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/curtains.html", HandleResponse);
     };
 
     UseAjax.LoadPelmets = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/pelmets.html", HandleResponse);
     };
 
     UseAjax.LoadMattresses = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/mattresses.html", HandleResponse);
     };
 
     UseAjax.LoadClothesAdjustments = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/clothesAdjustments.html", HandleResponse);
     };
 
     UseAjax.LoadUpholstery = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/upholstery.html", HandleResponse);
     };
 
     UseAjax.LoadCarpets = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/carpets.html", HandleResponse);
     };
 
     UseAjax.LoadPvc = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/pvc.html", HandleResponse);
     };
 
     UseAjax.LoadFeatherPurification = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/featherPurification.html", HandleResponse);
     };
 
     UseAjax.LoadEmbroidery = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/embroidery.html", HandleResponse);
     };
 
     UseAjax.LoadTentReparations = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/tentReparations.html", HandleResponse);
     };
 
     UseAjax.LoadRollerBlinds = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/rollerBlinds.html", HandleResponse);
     };
 
     UseAjax.LoadGardenFurniture = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/gardenFurniture.html", HandleResponse);
     };
 
     UseAjax.LoadCarUpholstery = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/carUpholstery.html", HandleResponse);
     };
 
     UseAjax.LoadAntiques = function () {
+        ShowLoading();
         $AjaxUtils.SendGetRequest("./snippets/antiques.html", HandleResponse);
     };
 
